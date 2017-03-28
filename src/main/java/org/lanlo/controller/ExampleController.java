11165db3841c2bf23c3c90c76c523fcad297089a
loki.lan <loki.lan@oocl.com>
2017-03-28 10:33:33 +0800

    remove unused import

--- src/main/java/org/lanlo/controller/ExampleController.java
+++ src/main/java/org/lanlo/controller/ExampleController.java
@@ -1,6 +1,5 @@
 package org.lanlo.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
